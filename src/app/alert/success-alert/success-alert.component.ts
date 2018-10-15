fed2b064be4851e64fb49de7589e8f946b9dc72c
stefanpuhalo <stefanphl@gmail.com>
2018-10-16 01:03:49 +0200

    component initialization

---
+++ src/app/alert/success-alert/success-alert.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+
+export class SuccessAlertComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+
+
+}
+
